6325192a74c1f86cfbe7b4713f7342266b6a17f7
kidzcartoons <135835628+kidzcartoons@users.noreply.github.com>
2026-08-02 06:10:09 +0500

    Add new redirect links to linksDB

--- api/redirect.js
+++ api/redirect.js
@@ -1,8 +1,14 @@
 const linksDB = {
-  '9v3EHVbg': 'https://gplinks.co/Y8WPef1F',
+  'fXsCW834': 'https://gplinks.co/nX4dOcy?skip_sub=1',
   'DuCF93bt': 'https://gplinks.co/Y8WPef1F?skip_sub=1',
-  'XPdgBN': 'https://gplinks.co/XPdgBN',
-  'P2APl': 'https://gplinks.co/P2APl'
+  'VJu41BDO': 'https://gplinks.co/gJfe7H6U?skip_sub=1',
+  'z9oFNvDl': 'https://gplinks.co/67y7mAEZ?skip_sub=1',
+  
+  'NrIGXUx3': 'https://gplinks.co/krMGvia1?skip_sub=1',
+  '1iv0dd2e': 'https://gplinks.co/C7liA?skip_sub=1',
+  'ZylYAObk': 'https://gplinks.co/IyN1mJPp?skip_sub=1',
+  'xSQ4aoIP': 'https://gplinks.co/qn1iD?skip_sub=1',
+  
 };
 
 export default function handler(req, res) {
